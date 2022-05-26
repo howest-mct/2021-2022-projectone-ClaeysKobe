f4e8d230a41a39433cdab47b0d9739c8e6ab61db
ClaeysKobe <kobe.claeys@student.howest.be>
2022-05-26 19:01:35 +0200

    fix: app.js via regions

--- front-end/script/app.js
+++ front-end/script/app.js
@@ -1,8 +1,12 @@
 'use strict';
+// #region ***  DOM references                           ***********
 let htmlBoxOpen, htmlBoxClose, htmlHistoryToday, htmlHistoryAll,  htmlLettersToday, htmlLidStatus, historyToday, historyAll
 
 const lanIP = `${window.location.hostname}:5000`;
 const socket = io(`http://${lanIP}`);
+// #endregion
+
+// #region ***  Callback-Visualisation - show___         ***********
 
 const showHistoryToday = function(jsonObject) {
   // console.log(jsonObject.sensors);
@@ -52,7 +56,12 @@ const showLetters = function(payload) {
     htmlLettersToday.innerHTML = '--';
   };
 };
+// #endregion
 
+// #region ***  Callback-No Visualisation - callback___  ***********
+// #endregion
+
+// #region ***  Data Access - get___                     ***********
 const loadHistoryToday = function () {
   const url = `http://192.168.168.169:5000/api/v1/events/today/`;
   handleData(url, showHistoryToday);
@@ -61,18 +70,20 @@ const loadHistoryToday = function () {
 const loadHistoryAll = function () {
   const url = `http://192.168.168.169:5000/api/v1/events/`;
   handleData(url, showHistoryAll);
-}
+};
 
 const loadLidStatus = function () {
   const url = `http://192.168.168.169:5000/api/v1/events/lid/`;
   handleData(url,showLidStatus);
-}
+};
 
 const loadLettersToday = function () {
   const url = `http://192.168.168.169:5000/api/v1/events/letters/`;
   handleData(url,showLetters);
-}
+};
+// #endregion
 
+// #region ***  Event Listeners - listenTo___            ***********
 const listenToUI = function () {
   // togle box lock
     htmlBoxOpen.addEventListener('click', function () {
@@ -142,8 +153,10 @@ const listenToSocket = function () {
     }
   })
 };
+// #endregion
 
-document.addEventListener("DOMContentLoaded", function () {
+// #region ***  Init / DOMContentLoaded                  ***********
+const init = function(){
   console.info("DOM geladen");
   htmlBoxOpen = document.querySelector('.js-boxopen');
   htmlBoxClose = document.querySelector('.js-boxclose');
@@ -156,4 +169,7 @@ document.addEventListener("DOMContentLoaded", function () {
   loadHistoryToday();
   loadLidStatus();
   loadLettersToday();
-});
+}
+
+document.addEventListener('DOMContentLoaded',init);
+// #endregion
